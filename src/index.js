6cc8528e507b3035a025f3dd7c0060b2c585bf70
OlegShinka <volkonxxl@gmail.com>
2024-01-24 20:38:13 +0200

    react-create-app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import data from "./data.json";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = document.getElementById("root");
+
+const Head = () => {
+  return <p> I am head</p>;
+};
+const Card = () => {
+  return data.map((photo) => {
+    return (
+      <div
+        key={photo.id}
+        className="card mx-auto my-2"
+        style={{ width: "18rem" }}
+      >
+        <img src={photo.url} className="card-img-top" alt={photo.title} />
+        <div className="card-body">
+          <Head />
+          <h5 className="card-title">Card title: {photo.title}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="go" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    );
+  });
+};
+
+ReactDOM.createRoot(root).render(<Card />);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<React.StrictMode></React.StrictMode>);
